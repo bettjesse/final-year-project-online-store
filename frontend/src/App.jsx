25c3459107a61c4a1557ad0da27b5eccb8df1868
jesse <jessebett1999@gmail.com>
2023-02-13 08:12:06 +0300

    week-1 basic UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=' bg-black text-red-300 text-3xl' >
+    green 
+    </div>
+  )
+}
+
+export default App
